9c35194f5371dd3c0326e0acbbbca4bb83e8649d
Joe Nelson <cred+github@begriffs.com>
2014-03-27 23:46:17 -0400

    Send textarea keyup events through websocket

--- web.js
+++ web.js
@@ -14,14 +14,13 @@ console.log('http server listening on %d', port);
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  });
-  }, 1000);
-
   console.log('websocket connection open');
 
   ws.on('close', function() {
     console.log('websocket connection close');
-    clearInterval(id);
+  });
+
+  ws.on('message', function(s) {
+    console.log(s);
   });
 });
