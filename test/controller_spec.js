26c33f70e7e859e390e17c0f1fb43a8cd851999d
Joe Nelson <cred+github@begriffs.com>
2014-03-28 21:10:52 -0400

    WIP: text changes from remote keystrokes

---
+++ test/controller_spec.js
@@ -0,0 +1,12 @@
+var request = require('supertest')
+  , app = require('../web')
+  , assert = require('chai').assert;
+
+describe("Socket fun", function () {
+
+  xit("sdfkjd", function () {
+    request(app)
+    .get('')
+  })
+
+});
--- web.js
+++ web.js
@@ -2,25 +2,23 @@ var WebSocketServer = require('ws').Server
   , http = require('http')
   , express = require('express')
   , app = express()
-  , port = process.env.PORT || 5000;
+  , port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
-server.listen(port);
-
+var io = require('socket.io').listen(server, {origins: '*:*'});
 console.log('http server listening on %d', port);
 
-var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
-wss.on('connection', function(ws) {
-  console.log('websocket connection open');
-
-  ws.on('close', function() {
-    console.log('websocket connection close');
+io.on('connection', function(ws) {
+  console.log("CONNECTED")
+  ws.on('disconnect', function() {
+    console.log('websocket connection %d close', clients[ws]);
   });
 
-  ws.on('message', function(s) {
-    console.log(s);
+  ws.on('edit', function(s) {
+    ws.broadcast.emit('test', s);
   });
 });
+server.listen(port);
