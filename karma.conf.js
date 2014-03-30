35c78b0f7994909eb91c880d1abbbee0a17df7c3
Joe Nelson <cred+github@begriffs.com>
2014-03-30 19:31:31 -0400

    Requirejs name change

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/test-main.js',
+      'test/requirejs-config.js',
       {pattern: 'public/**/*.js', included: false},
       {pattern: 'test/client/*.js', included: false}
     ],
