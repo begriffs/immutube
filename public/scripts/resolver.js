08b99706ef8bda6992eadd2aa2d057ff1d672cdf
Joe Nelson <cred+github@begriffs.com>
2014-03-31 00:26:30 -0700

    More resolver tests
    Looks like resolve is precisely slice

--- public/scripts/resolver.js
+++ public/scripts/resolver.js
@@ -1,7 +1,5 @@
 define([], function () {
   return function (corpus, sel_start, sel_end, edit) {
-    if(sel_start === sel_end) {
-      return corpus.slice(0, sel_start) + edit + corpus.slice(sel_start);
-    }
+    return corpus.slice(0, sel_start) + edit + corpus.slice(sel_end);
   };
 });
--- test/unit/resolver_spec.js
+++ test/unit/resolver_spec.js
@@ -1,17 +1,18 @@
 define(['resolver', 'lodash'], function(r, _) {
 
-  function sequentialResolution(s) {
+  function sequentialResolution(corpus, s, start_pos) {
+    start_pos = start_pos || 0;
     return _.reduce(s, function (corpus, c, i) {
-      return r(corpus, i, i, c);
-    }, '');
+      return r(corpus, start_pos+i, start_pos+i, c);
+    }, corpus);
   }
 
-  describe('resolver', function() {
-    it('can add a letter', function() {
+  describe('resolver', function () {
+    it('can add a letter', function () {
       assert.equal(r('', 0, 0, 'x'), 'x');
     });
 
-    it('resolves two requests to add a letter at start', function() {
+    it('resolves two requests to add a letter at start', function () {
       assert.equal('yx',
         r(
           r('', 0, 0, 'x'),
@@ -20,8 +21,30 @@ define(['resolver', 'lodash'], function(r, _) {
       );
     });
 
-    it('can add a sequence of letters', function() {
-      assert.equal(sequentialResolution('abc'), 'abc');
+    it('can add a sequence of letters', function () {
+      assert.equal(sequentialResolution('', 'abc'), 'abc');
+    });
+
+    it('can add a shifted sequence of letters', function () {
+      assert.equal('axyzbc',
+        sequentialResolution(
+          sequentialResolution('', 'abc'),
+          'xyz', 1
+        )
+      );
+    });
+
+    it('shifts edits backward if corpus is smaller', function () {
+      assert.equal('ab',
+        r(
+          r('', 7,  7,  'a'),
+                12, 12, 'b'
+        )
+      );
+    });
+
+    it('replaces a range', function () {
+      assert.equal('axc', r('abc', 1, 2, 'x'));
     });
   });
 
