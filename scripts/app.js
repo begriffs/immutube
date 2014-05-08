884f63ae23ab61e68e503759cb4a6965bf79cc82
Joe Nelson <cred+github@begriffs.com>
2014-05-07 19:23:55 -0700

    Blank slate

--- scripts/app.js
+++ scripts/app.js
@@ -5,30 +5,20 @@ define([
 , 'pointfree'
 , 'Maybe'
 , 'player'
-, 'youtube'
 , 'bacon'
-], function($, _, P, Maybe, Player, youtube, bacon) {
+], function($, _, P, Maybe, Player, bacon) {
   'use strict';
 
-  // some "move out over here" helpers
+  // HELPERS ///////////////////////////////////////////////////////////////////////////
   var compose = P.compose;
   var map = P.map;
-  var log = function(x){ console.log(x); return x;}
-  var fork = _.curry(function(f, future) { return future.fork(log, f) })
+  var log = function(x) { console.log(x); return x; }
+  var fork = _.curry(function(f, future) { return future.fork(log, f); })
   var setHtml = _.curry(function(sel, x) { return $(sel).html(x); });
 
 
-  // setup youtube search li stream
-  var toParam = function (x) { return {q: x.target.value}; };
-  var getResults = compose(youtube, toParam)
-  var youTubeStream = Bacon.fromEventTarget($("#search"), "keydown").debounce(300).map(getResults);
+  // PURE //////////////////////////////////////////////////////////////////////////////
 
-  // setup click to player stream
-  var toYoutubeId = function(e){ return $(e.target).data('youtubeid'); }
-  var makePlayer = compose(map(Player.create), Maybe, toYoutubeId)
-  var playerStream = Bacon.fromEventTarget(document, "click").map(makePlayer);
+  // IMPURE ////////////////////////////////////////////////////////////////////////////
 
-  // run app
-  youTubeStream.onValue(fork(setHtml('#results')));
-  playerStream.onValue(map(setHtml('#player')));
 });
