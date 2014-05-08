2640eebb4d1ead524bdfff703b752f54f775b2cf
Joe Nelson <cred+github@begriffs.com>
2014-05-07 20:01:45 -0700

    Create search event stream and log it

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,11 @@ define([
 , 'pointfree'
 , 'Maybe'
 , 'player'
+, 'io'
 , 'bacon'
-], function($, _, P, Maybe, Player, bacon) {
+], function($, _, P, Maybe, Player, io, bacon) {
   'use strict';
+  io.extendFn();
 
   // HELPERS ///////////////////////////////////////////////////////////////////////////
   var compose = P.compose;
@@ -15,10 +17,15 @@ define([
   var log = function(x) { console.log(x); return x; }
   var fork = _.curry(function(f, future) { return future.fork(log, f); })
   var setHtml = _.curry(function(sel, x) { return $(sel).html(x); });
-
+  var listen = _.curry(function (event, target) {
+    return bacon.fromEventTarget(target, event);
+  });
 
   // PURE //////////////////////////////////////////////////////////////////////////////
 
-  // IMPURE ////////////////////////////////////////////////////////////////////////////
+  //+ search :: Selector -> IO EventStream String
+  var getInputStream = compose(map(listen('keyup')), $.toIO());
 
+  // IMPURE ////////////////////////////////////////////////////////////////////////////
+  io.runIO(getInputStream('#search')).onValue(log);
 });
